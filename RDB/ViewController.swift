9e0adc974a1e51dfac91ee05b1c29e47dd1192f3
ZhagaSA <79198232376@yandex.ru>
2021-07-10 00:59:29 +0300

    save code

--- RDB/ViewController.swift
+++ RDB/ViewController.swift
@@ -9,11 +9,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var viewColor: UIView!
+    
+    @IBOutlet weak var redSlider: UISlider! {
+        didSet {
+            redSlider.tintColor = UIColor.red
+        }
+    }
+    @IBOutlet weak var greenSlider: UISlider! {
+    didSet {
+        greenSlider.tintColor = UIColor.green
+    }
+    }
+    @IBOutlet weak var blueSlider: UISlider! {
+     didSet {
+        blueSlider.tintColor = UIColor.blue
+    }
+    }
+    @IBOutlet weak var rNumber: UILabel!
+    @IBOutlet weak var gNumber: UILabel!
+    @IBOutlet weak var bNumber: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    
+    func changeView() {
+        viewColor.backgroundColor = UIColor(displayP3Red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+    }
+    @IBAction func sliderAction(_ sender: Any) {
+        changeView()
+        rNumber.text = String(redSlider.value)
+        gNumber.text = String(greenSlider.value)
+        bNumber.text = String(blueSlider.value)
+        
+    
+    
 }
-
+}
